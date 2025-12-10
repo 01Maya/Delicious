b71ce318fba063be5ae11c9cd64ee43bc3e0a56b
Mansi_Patel <patelmaya9812@gmail.com>
2025-12-11 00:01:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const playfair = Playfair_Display({
 })
 
 export const metadata: Metadata = {
-  title: "Delizioso - Fresh & Delicious for Every Bite",
+  title: "Delizioso - Fresh & Delicious for Every Bite🍕",
   description:
     "Experience authentic flavors with our fresh, handcrafted dishes made with love and premium ingredients.",
 }
